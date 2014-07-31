09da3fe98fb201f1c9dae7effd6c6d60b9f68ef8
Kevin <khuang6@ucsc.edu>
2014-07-31 16:44:11 -0700

    bouncing

--- src/main/webapp/onoff.js
+++ src/main/webapp/onoff.js
@@ -1,9 +1,13 @@
-//$(document).ajaxStop($.unblockUI); 
-
 $(document).ready(function(){
+
+    $(".activity").effect("bounce", { times:5, distance:300 }, 600);
+    //$(".service2").effect("bounce", { times:2, distance:200 }, 200);
+    //$(".arrows").effect("bounce", { times:4, distance:200 }, 400);
+
     var taxflag = 1;
     var shippingflag = 1;
     var promotionflag = 1;
+
     $(".taxservicecopy").click(function(){
         if(taxflag === 1){
             $.blockUI({ message: $('#taxserviceon') }); 
@@ -16,7 +20,7 @@ $(document).ready(function(){
             taxflag = 1;
         }
     });
-     $(".shippingservice").click(function(){
+    $(".shippingservice").click(function(){
         if(shippingflag === 1){
             $.blockUI({ message: $('#shippingserviceon') }); 
             setTimeout($.unblockUI, 2000); 
@@ -28,7 +32,7 @@ $(document).ready(function(){
             shippingflag = 1;
         }
     });
-      $(".promotionservice").click(function(){
+    $(".promotionservice").click(function(){
         if(promotionflag === 1){
             $.blockUI({ message: $('#promotionserviceon') }); 
             setTimeout($.unblockUI, 2000); 
